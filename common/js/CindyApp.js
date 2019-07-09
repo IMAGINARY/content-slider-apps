c8c333996e31a8d0b24a51a197aaf5345659d5a5
Christian Stussak <stussak@mfo.de>
2019-07-09 14:28:09 +0200

    Factor out CindyApp class for dealing with CindyJS apps in a generic way

---
+++ common/js/CindyApp.js
@@ -0,0 +1,109 @@
+import Application from './application.js';
+
+class CindyApp extends Application {
+    constructor() {
+        super();
+        console.log("CindyApp called");
+        this._cindy = null;
+        this._cindyPromise = (async () => createCindy.newInstance(await this.cindyArgs(this.canvas)))()
+            .then(cindy => this._cindy = cindy);
+        this._isReady = false;
+        this._readyPromise = CindyApp.waitForDomInsertion(this.canvas, document)
+            .then(() => this.cindyReady)
+            .then(cindy => {
+                cindy.startup();
+                this._isReady = true;
+                return this;
+            });
+    }
+
+    static async loadScripts(prefix, infixes, suffix) {
+        const scripts = {};
+        const promises = [];
+        // TODO: error handling for failed loads
+        for (let infix of infixes)
+            promises.push($.get(prefix + infix + suffix).then(scriptText => scripts[infix] = scriptText));
+        Promise.all(promises);
+        return scripts;
+    }
+
+    get canvas() {
+        if (typeof this._canvas === 'undefined') {
+            this._canvas = document.createElement('canvas');
+            this._canvas.width = 1560;
+            this._canvas.height = 1170;
+        }
+        return this._canvas;
+    }
+
+    get cindyReady() {
+        return this._cindyPromise;
+    }
+
+    get cindy() {
+        return this._cindy;
+    }
+
+    async cindyArgs(canvas) {
+        throw new Error('Unimplemented');
+    }
+
+    static async waitForDomInsertion(element, root) {
+        return new Promise(resolve => {
+            if (element.parentElement === null) {
+                // resolve after insertion of the element into the given DOM subtree
+                const observer = new MutationObserver(() => {
+                    if (element.parentElement !== null) {
+                        observer.disconnect();
+                        resolve();
+                    }
+                });
+                observer.observe(root, {childList: true, subtree: true});
+            } else {
+                // resolve immediately
+                resolve();
+            }
+        });
+    }
+
+    get ready() {
+        console.log(this._readyPromise);
+        return this._readyPromise;
+    }
+
+    get domElement() {
+        return this.canvas;
+    }
+
+    get name() {
+        return 'CindyApp';
+    }
+
+    get description() {
+        return 'Eine App, die mit CindyJS erstellt wurde.';
+    }
+
+    get credits() {
+        return '';
+    }
+
+    pause() {
+        console.log(`pause ${this.constructor.name}`);
+        if (this._isReady)
+            this.cindy.evokeCS('pause();');
+    }
+
+    resume() {
+        console.log(`resume ${this.constructor.name}`);
+        if (this._isReady)
+            this.cindy.evokeCS('resume();');
+    }
+
+    restart(pauseAfterRestart) {
+        console.log(`restart ${this.constructor.name}`);
+        if (this._isReady)
+            this.cindy.evokeCS('reset();');
+    }
+}
+
+export default CindyApp;
--- image-spiral/ImageSpiralApp.js
+++ image-spiral/ImageSpiralApp.js
@@ -1,149 +1,109 @@
-import Application from '../common/js/application.js';
+import CindyApp from "../common/js/CindyApp.js";
 
-class ImageSpiralApp extends Application {
+class ImageSpiralApp extends CindyApp {
     constructor() {
         super();
+    }
 
-        const canvas = document.createElement('canvas');
-        canvas.width = 1560;
-        canvas.height = 1170;
-
+    async cindyArgs(canvas) {
         const relativeUrl = filename => new URL(filename, import.meta.url);
-
-        const initScriptPromise = $.get(relativeUrl('ImageSpiral_init.cs'));
-        const drawScriptPromise = $.get(relativeUrl('ImageSpiral_draw.cs'));
-        const tickScriptPromise = $.get(relativeUrl('ImageSpiral_tick.cs'));
-        const mousedownScriptPromise = $.get(relativeUrl('ImageSpiral_mousedown.cs'));
-
-        this._readyPromise = new Promise(async (resolve) => {
-            this._cindy = createCindy.newInstance({
-                scripts: {
-                    init: await initScriptPromise,
-                    mousedown: await mousedownScriptPromise,
-                    draw: await drawScriptPromise,
-                    tick: await tickScriptPromise,
+        return {
+            scripts: await CindyApp.loadScripts(relativeUrl('ImageSpiral_'), ['init', 'mousedown', 'draw', 'tick'], '.cs'),
+            defaultAppearance: {},
+            images: {
+                test1: relativeUrl('assets/test1.png'),
+                test2: relativeUrl('assets/test2.png'),
+                test3: relativeUrl('assets/test3.png'),
+                test4: relativeUrl('assets/test4.png'),
+                test5: relativeUrl('assets/test5.png'),
+                test6: relativeUrl('assets/test6.png'),
+            },
+            ports: [{
+                element: canvas,
+                background: "rgba(0,255,0,0)",
+                transform: [{visibleRect: [-12, 16, 30, -14]}]
+            }],
+            geometry: [
+                {
+                    name: "B",
+                    type: "Free",
+                    pos: [4, -0.3346613545816733, 0.39840637450199207],
+                    size: 9,
+                    color: [0, 1, 0]
                 },
-                defaultAppearance: {},
-                images: {
-                    test1: relativeUrl('assets/test1.png'),
-                    test2: relativeUrl('assets/test2.png'),
-                    test3: relativeUrl('assets/test3.png'),
-                    test4: relativeUrl('assets/test4.png'),
-                    test5: relativeUrl('assets/test5.png'),
-                    test6: relativeUrl('assets/test6.png'),
+                {
+                    name: "C",
+                    type: "Free",
+                    pos: [4, 1.2289387168822175, 0.4359319522866157],
+                    size: 9,
+                    color: [0, 1, 0]
                 },
-                ports: [{
-                    element: canvas,
-                    background: "rgba(0,255,0,0)",
-                    transform: [{visibleRect: [-12, 16, 30, -14]}]
-                }],
-                geometry: [
-                    {
-                        name: "B",
-                        type: "Free",
-                        pos: [4, -0.3346613545816733, 0.39840637450199207],
-                        size: 9,
-                        color: [0, 1, 0]
-                    },
-                    {
-                        name: "C",
-                        type: "Free",
-                        pos: [4, 1.2289387168822175, 0.4359319522866157],
-                        size: 9,
-                        color: [0, 1, 0]
-                    },
-                    {
-                        name: "D",
-                        type: "Free",
-                        pos: [4, 1.9, 0.17953321364452424],
-                        pinned: true,
-                        size: 7,
-                        color: [1, 1, 1],
-                        labeled: true,
-                        visible: false
-                    },
-                    {
-                        name: "C0",
-                        type: "CircleMr",
-                        args: ["D"],
-                        radius: 3,
-                        pinned: true,
-                        size: 3,
-                        color: [1, 1, 1],
-                        printname: "$C_{0}$"
-                    },
-                    {
-                        name: "E",
-                        type: "Free",
-                        pos: [5, 3.1, 0.20859302441322417],
-                        pinned: false,
-                        size: 9,
-                        color: [1, 1, 1]
-                    },
-                    {
-                        name: "A",
-                        type: "Free",
-                        pos: [4, 2, 0.14880952380952378],
-                        pinned: true,
-                        color: [1, 0, 0],
-                        labeled: true,
-                        visible: false
-                    },
-                    {
-                        name: "F",
-                        type: "Free",
-                        pos: [4, 1.1, 0.14880952380952378],
-                        pinned: true,
-                        color: [1, 0, 0],
-                        labeled: true,
-                        visible: false
-                    },
-                    {
-                        name: "a",
-                        type: "Segment",
-                        args: ["A", "F"],
-                        pos: [-0.1488095238095238, 0, 4],
-                        size: 3,
-                        color: [1, 1, 1],
-                        labeled: true
-                    },
-                    {
-                        name: "G",
-                        type: "PointOnLine",
-                        args: ["a"],
-                        pos: [4, 1.8, 0.1488095238095238],
-                        size: 9,
-                        color: [1, 1, 1],
-                        labeled: true
-                    }
-                ]
-            });
-            if (canvas.parentElement === null) {
-                // startup after insertion of the canvas into the DOM
-                const observer = new MutationObserver(() => {
-                    if (canvas.parentElement !== null) {
-                        this._cindy.startup();
-                        resolve(this);
-                        observer.disconnect();
-                    }
-                });
-                observer.observe(document, {childList: true, subtree: true});
-            } else {
-                // startup immediately
-                this._cindy.startup();
-                resolve(this);
-            }
-        });
-
-        this._canvas = canvas;
-    }
-
-    get ready() {
-        return this._readyPromise;
-    }
-
-    get domElement() {
-        return this._canvas;
+                {
+                    name: "D",
+                    type: "Free",
+                    pos: [4, 1.9, 0.17953321364452424],
+                    pinned: true,
+                    size: 7,
+                    color: [1, 1, 1],
+                    labeled: true,
+                    visible: false
+                },
+                {
+                    name: "C0",
+                    type: "CircleMr",
+                    args: ["D"],
+                    radius: 3,
+                    pinned: true,
+                    size: 3,
+                    color: [1, 1, 1],
+                    printname: "$C_{0}$"
+                },
+                {
+                    name: "E",
+                    type: "Free",
+                    pos: [5, 3.1, 0.20859302441322417],
+                    pinned: false,
+                    size: 9,
+                    color: [1, 1, 1]
+                },
+                {
+                    name: "A",
+                    type: "Free",
+                    pos: [4, 2, 0.14880952380952378],
+                    pinned: true,
+                    color: [1, 0, 0],
+                    labeled: true,
+                    visible: false
+                },
+                {
+                    name: "F",
+                    type: "Free",
+                    pos: [4, 1.1, 0.14880952380952378],
+                    pinned: true,
+                    color: [1, 0, 0],
+                    labeled: true,
+                    visible: false
+                },
+                {
+                    name: "a",
+                    type: "Segment",
+                    args: ["A", "F"],
+                    pos: [-0.1488095238095238, 0, 4],
+                    size: 3,
+                    color: [1, 1, 1],
+                    labeled: true
+                },
+                {
+                    name: "G",
+                    type: "PointOnLine",
+                    args: ["a"],
+                    pos: [4, 1.8, 0.1488095238095238],
+                    size: 9,
+                    color: [1, 1, 1],
+                    labeled: true
+                }
+            ]
+        };
     }
 
     get name() {
@@ -156,21 +116,6 @@ class ImageSpiralApp extends Application {
 
     get credits() {
         return 'Author 2a<br/>Author 2b';
-    }
-
-    pause() {
-        console.log(`pause ${this.constructor.name}`);
-        this.ready.then(instance => instance._cindy.evokeCS('pause();'));
-    }
-
-    resume() {
-        console.log(`resume ${this.constructor.name}`);
-        this.ready.then(instance => instance._cindy.evokeCS('reset();'));
-    }
-
-    restart(pauseAfterRestart) {
-        console.log(`restart ${this.constructor.name}`);
-        this.ready.then(instance => instance._cindy.evokeCS('reset();'));
     }
 }
 
