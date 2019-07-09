70ab0da30a7899b53c8e61f00f13f9be4e26b505
Christian Stussak <stussak@mfo.de>
2019-07-09 22:33:12 +0200

    Add tree app

---
+++ tree/TreeApp.js
@@ -0,0 +1,39 @@
+import CindyApp from "../common/js/CindyApp.js";
+
+class TreeApp extends CindyApp {
+    constructor() {
+        super();
+    }
+
+    async _initCindyArgs() {
+        const relativeUrl = filename => new URL(filename, import.meta.url);
+        return {
+            scripts: await CindyApp.loadScripts(relativeUrl('Tree_'), ['init', 'draw'], '.cs'),
+            ports: [{
+                element: this.canvas,
+                background: "rgba(0,0,0,0)",
+                transform: [{visibleRect: [-10, 10, 10, -10]}]
+            }],
+            geometry: [
+                {name: "A", type: "Free", size: 6, pos: [0, -8], color: [1, 1, 1]},
+                {name: "B", type: "Free", size: 6, pos: [0, -4.5], color: [1, 1, 1]},
+                {name: "C", type: "Free", size: 6, pos: [1.5, -2], color: [1, 1, 1]},
+                {name: "D", type: "Free", size: 6, pos: [-1.6, -2.5], color: [1, 1, 1]},
+            ]
+        };
+    }
+
+    get name() {
+        return 'Ein Baum';
+    }
+
+    get description() {
+        return 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.';
+    }
+
+    get credits() {
+        return '';
+    }
+}
+
+export default TreeApp;
---
+++ tree/Tree_draw.cs
@@ -0,0 +1,16 @@
+t1=map(A,B,B,C);
+t2=map(A,B,B,D);
+col(x):=(0.6*x,(1-x)*.8,0);
+
+
+tree(a,b,n):=(
+  if(n>0&|a_[1,2]/a_3,b_[1,2]/b_3|>.15,
+     draw(a,b,size->n*1*2,color->col(n/nn));
+     tree(t1*a,t1*b,n-1);
+     tree(t2*a,t2*b,n-1);
+     );
+  );
+nn=12;
+// drawimage((-15,-11),(15,-11),"back");
+
+tree(A.homog,B.homog,nn);
