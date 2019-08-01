7841e90f1f8736468d70278e26d9e11a678ec811
Aaron Montag <aaron.montag@imaginary.org>
2019-08-01 18:46:40 +0200

    Fix doppelpendel trace

--- double-pendulum/DoublePendulum_mousedown.cs
+++ double-pendulum/DoublePendulum_mousedown.cs
@@ -1,6 +1,6 @@
 mpo=mouse().xy;
 if(mpo.x>button1_1_1 & mpo.x<button1_2_1 & mpo.y>button1_1_2 & mpo.y<button1_3_2,
-if(pressed1,stopanimation(),playanimation());
+if(pressed1,stopanimation(),lastsimulationtime = 0; l=[]; playanimation());
 pressed1=!pressed1);
 if(mpo.x>button2_1_1 & mpo.x<button2_2_1 & mpo.y>button2_1_2 & mpo.y<button2_3_2,pressed2=!pressed2);
 
