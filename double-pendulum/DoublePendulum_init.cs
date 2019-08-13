06f14e317671e8f1a5c516787b9669391c3f1c3c
Aaron Montag <aaron.montag@imaginary.org>
2019-08-13 17:33:00 +0200

    Add idle animation for DoublePendulum

--- double-pendulum/DoublePendulum_init.cs
+++ double-pendulum/DoublePendulum_init.cs
@@ -1,11 +1,12 @@
 button1=[[14+9,7],[20+9,7],[20+9,8.7],[14+9,8.7]];
 button2=[[14+9,4],[20+9,4],[20+9,5.7],[14+9,5.7]];
-pressed1=false;
-pressed2=false;
+pressed1=true;
+pressed2=true;
 
 oldb=B.xy;
 oldc=C.xy;
 
+playanimation();
 
 n=300;
 lastsimulationtime = 0;
@@ -19,12 +20,15 @@ C.homog=(9, 7, 1);
 F.homog=(3.4, -4, -0.5);
 G.homog=(4, -2.1882352, -0.588235);
 l=[];
-pressed1=false;
-pressed2=false;
+pressed1=true;
+pressed2=true;
+playanimation();
+
     );
 
 pause():=(
   pauseanimation();
+  l = [];
 );
 resume():=(
   if(pressed1, lastsimulationtime = 0; l=[]; playanimation(););
