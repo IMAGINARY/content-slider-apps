276dba027f2bcb6f35dbb420b76125b14fedba01
Aaron Montag <aaron.montag@imaginary.org>
2019-08-22 10:37:49 +0200

    Rework puase/resume for DoublePendulum

--- double-pendulum/DoublePendulum_init.cs
+++ double-pendulum/DoublePendulum_init.cs
@@ -1,15 +1,12 @@
 button1=[[14+9,7],[20+9,7],[20+9,8.7],[14+9,8.7]];
 button2=[[14+9,4],[20+9,4],[20+9,5.7],[14+9,5.7]];
 
-pressed1=true;
-pressed2=true;
+
 
 oldb=B.xy;
 oldc=C.xy;
 
-l = [];
 n=300;
-lastsimulationtime = 0;
 
 reset():=(
   resetclock();
@@ -21,21 +18,21 @@ reset():=(
   F.homog=(3.4, -4, -0.5);
   G.homog=(4, -2.1882352, -0.588235);
   l=[];
-  //pressed1=true;
-  //pressed2=false;
+  pressed1=true;
+  pressed2=true;
 );
+reset();
 
 pause():=(
-  l = [];
-  //pressed1=false;
   pauseanimation();
 );
 
 resume():=(
-  playanimation();
-  //pressed1 = true;
-  //pressed2 = true;
-  lastsimulationtime = 0;
-  resetclock();
-  playanimation();
+  if(pressed1,
+    lastsimulationtime = 0;
+    resetclock();
+    playanimation();
+    ,
+    pauseanimation()
+  );
 );
--- double-pendulum/DoublePendulum_mousedown.cs
+++ double-pendulum/DoublePendulum_mousedown.cs
@@ -1,10 +1,10 @@
 mpo=mouse().xy;
 if(mpo.x>button1_1_1 & mpo.x<button1_2_1 & mpo.y>button1_1_2 & mpo.y<button1_3_2,
-  if(pressed1,
-    stopanimation(),
-    lastsimulationtime = 0; l=[]; playanimation();
-  );
   pressed1=!pressed1;
+  if(pressed1,
+    resume(),
+    pause()
+  );
 );
 if(mpo.x>button2_1_1 & mpo.x<button2_2_1 & mpo.y>button2_1_2 & mpo.y<button2_3_2,pressed2=!pressed2);
 
