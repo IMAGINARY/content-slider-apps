d6952472aa4c57518dbabd2a905d6c442c9ea06f
Aaron Montag <aaron.montag@imaginary.org>
2019-09-03 17:15:29 +0200

    Add different starting positions to solitaire

--- solitaire/Solitaire_init.cs
+++ solitaire/Solitaire_init.cs
@@ -1,3 +1,36 @@
+randomfield() := (
+  k = randomint(3);
+  if(k==0,
+    //Triangular
+    unit=4.54;
+    pegs=[[17.88,-11],[8.8,4.72],[-0.28,-11],[4.26,-3.14],[13.34,-3.14],[15.61,-7.07],[8.8,-3.14],[8.8,-11],[6.53,-7.07],[1.99,-7.07],[4.26,-11],[13.34,-11],[6.53,0.79],[11.07,0.79],[11.07,-7.07]] ;
+    active=pegs--[pegs_(randomint(15)+1)];
+  );
+
+  if(k==1,
+    // English style
+    unit = 3.5;
+    pegs = (directproduct(-3..3,-1..1)++directproduct(-1..1,(-3..-2)++2..3));
+    active = pegs -- [(0,0)];
+    delta = (2,-.5);
+    pegs = unit*apply(pegs, #+delta);
+    active = unit*apply(active, #+delta);
+  );
+  if(k==2,
+    // French (European) style, 37 holes, 17th century;
+    unit = 3.5;
+    pegs = (directproduct(-3..3,-1..1)++directproduct(-1..1,(-3..-2)++2..3)++directproduct([-2,2],[-2,2]));
+    start = [1,0];
+    if(random()>0.5, start=-start);
+    if(random()>0.5, start=start_[2,1]);
+    active = pegs -- [start];
+    delta = (2,-.5);
+    pegs = unit*apply(pegs, #+delta);
+    active = unit*apply(active, #+delta);
+  );
+);
+
+
 upaction():=(
 if(pressed,reset());
     pressed=false;
@@ -41,18 +74,19 @@ animating=false;
 //KNOPF POSITION
 rect=[[23,-4],[26,-4],[26,-1],[23,-1]];
 pressed=false;
-unit=4.54;
-pegs=[[17.88,-11],[8.8,4.72],[-0.28,-11],[4.26,-3.14],[13.34,-3.14],[15.61,-7.07],[8.8,-3.14],[8.8,-11],[6.53,-7.07],[1.99,-7.07],[4.26,-11],[13.34,-11],[6.53,0.79],[11.07,0.79],[11.07,-7.07]] ;
+
 moving=false;
   pos=(0,0);
 
-active=pegs--[pegs_4];
+randomfield();
+
 special=[];
 drawpeg(p):=(
- gsave();
+ /*gsave();
  clip(circle(p+(0.2,-0.2),2.4));
  drawimage(A,B,"boardB");
- grestore();
+ grestore();*/
+ fillcircle(p,unit/2,color->[1,.3,0], alpha->.5);
 );
 
 drawspecial(pp):=(
@@ -72,6 +106,8 @@ drawspecial(pp):=(
     drawimage(A+diff,B+diff,"boardAX");
     fill(circle(special+diff,1.5),color->(0,0,0),alpha->.1);
     grestore();
+
+    fillcircle(pp,unit/2,color->[1,.3,.5], alpha->.5);
   );
 );
 
@@ -147,8 +183,8 @@ stillpossible():=(
 reset():=(
 
 err("RESET");
-   pegs=[[17.88,-11],[8.8,4.72],[-0.28,-11],[4.26,-3.14],[13.34,-3.14],[15.61,-7.07],[8.8,-3.14],[8.8,-11],[6.53,-7.07],[1.99,-7.07],[4.26,-11],[13.34,-11],[6.53,0.79],[11.07,0.79],[11.07,-7.07]] ;
-   active=pegs--[pegs_(randomint(15)+1)];
+   //pegs=[[17.88,-11],[8.8,4.72],[-0.28,-11],[4.26,-3.14],[13.34,-3.14],[15.61,-7.07],[8.8,-3.14],[8.8,-11],[6.53,-7.07],[1.99,-7.07],[4.26,-11],[13.34,-11],[6.53,0.79],[11.07,0.79],[11.07,-7.07]] ;
+   randomfield();
    special=[];
 )
 
