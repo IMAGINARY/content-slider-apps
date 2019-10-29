dbf9101188775ec7b5d9843f3bd15725828559fa
Aaron Montag <aaron.montag@imaginary.org>
2019-10-29 13:43:00 +0100

    Add resetbutton to Double-Pendulum

--- double-pendulum/DoublePendulum_init.cs
+++ double-pendulum/DoublePendulum_init.cs
@@ -1,5 +1,6 @@
 button1=[[14+9,7],[20+9,7],[20+9,8.7],[14+9,8.7]];
 button2=[[14+9,4],[20+9,4],[20+9,5.7],[14+9,5.7]];
+button3=[[14+9,1],[20+9,1],[20+9,2.7],[14+9,2.7]];
 
 
 
@@ -29,13 +30,19 @@ triggerinternalanimation():=(
   );
 );
 
-reset():=(
+resetparams():=(
   A.homog=(4, -2.40740, 0.462962);
   B.homog=(8, 4, 1);
   C.homog=(9, 7, 1);
   F.homog=(3.4, -4, -0.5);
   G.homog=(4, -2.1882352, -0.588235);
+  B.v = [0,0];
+  C.v = [0,0];
   l=[];
+);
+
+reset():=(
+  resetparams();
   pressed1=true;
   pressed2=true;
   triggerinternalanimation();
